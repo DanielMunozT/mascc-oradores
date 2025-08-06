446095e851eeca3e0e649efdf3d3e5e03bdd51d7
Daniel Munoz <skunk.earwig-0n@icloud.com>
2025-08-06 12:58:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -383,10 +383,10 @@ function renderEventsList(events, startDateInput, endDateInput) {
     html.push('<ol>');
     w.events.forEach(e => {
       html.push(
-        `<li>${e.event} - ${e.speaker} (<a href="${e.calendarUrl}" target="_blank">${T.calendar}</a>) ${formatShortRange(
+        `<li>${e.event}. ${e.speaker} (<a href="${e.calendarUrl}" target="_blank">${T.calendar}</a>) (${formatShortRange(
           e.start,
           e.end
-        )}</li>`
+        )})</li>`
       );
     });
     html.push('</ol>');
